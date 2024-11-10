b606202f6e52d3f84168a0778d3cbc0ca60b235e
Fabricio Zulian <fabriciozulian@outlook.com>
2024-11-09 21:57:26 -0300

    Adicionando resoluções dos exercícios de arrey em js

--- Arrey/playground.js
+++ Arrey/playground.js
@@ -49,3 +49,55 @@ function mapSemThis(arr){
 const nums2 = [2, 4, 6, 8, 10];
 
 console.log(mapSemThis(nums2));
+
+
+//Filtre e retorne todos os números pares de um array
+
+function filtraPares(arr){
+    return arr.filter(callback)
+}
+
+function callback(item){
+    return item % 2 === 0;
+}
+
+const MeuArrey = [1, 2, 4, 6, 8, 3, 44];
+
+
+console.log(filtraPares(MeuArrey));
+
+//Some todos os números de um array
+
+function somarArrey(arr){
+        return arr.reduce(function(prev, current){
+            return prev + current;
+        });
+    }
+
+
+console.log(somarArrey(MeuArrey));
+
+
+//Crie uma função que recebe uma lista de preços e um número representando o saldo disponível. Calcule qual será o saldo final após subtrair todos os preços da lista enviada.
+
+const brinquedos = [
+    {
+        name: 'carro',
+        preco: 20,
+    },
+    {
+        name: 'boneca',
+        preco: 30,
+    }
+];
+
+const saldo = 100;
+
+function retornaValorPosCompra(lista, saldo){
+   return lista.reduce(function(acomulador,posterior){
+        return acomulador - posterior.preco;
+
+    }, saldo);
+}
+
+console.log(retornaValorPosCompra(brinquedos, saldo));
