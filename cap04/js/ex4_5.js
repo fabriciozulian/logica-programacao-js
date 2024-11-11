4d7ec894d61397a91a5a3155528d8d36827dd375
Fabricio Zulian <fabriciozulian@outlook.com>
2024-11-10 22:14:11 -0300

    Ajustando resolução do exercicio 4_5

--- cap04/js/ex4_5.js
+++ cap04/js/ex4_5.js
@@ -3,5 +3,5 @@ const frm = document.querySelector("form");
 frm.addEventListener("submit", (e) =>{
     e.preventDefault();
     let resultado = Number.isInteger(Math.sqrt(Number(frm.inNumero.value)));
-    resultado === true ? document.getElementById("inResultado").innerText =`O número ${frm.inNumero.value} é uma raiz`: document.getElementById("inResultado").innerText = `O Número ${frm.inNumero.value} não é uma raiz`;
+    resultado === true ? document.getElementById("inResultado").innerText =` ${Math.sqrt(frm.inNumero.value)}`: document.getElementById("inResultado").innerText = `O Número ${frm.inNumero.value} não é uma raiz`;
 })
