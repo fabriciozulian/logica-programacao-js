bc18ccb68e90ac002c6640e1d521e7a36006d7c1
Fabricio Zulian <fabriciozulian@outlook.com>
2025-03-18 07:47:22 -0300

    Finalizando o exercicio 7_8

--- cap07/js/ex7_8.js
+++ cap07/js/ex7_8.js
@@ -1,12 +1,41 @@
 const frm = document.querySelector("form");
-const result = document.querySelector("InResultado");
+const result = document.querySelector("#inResultado");
+
+const taxaMulta = 0.02;
+const taxaJuros = 0.007;
 
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const valor = Number(frm.inValor.value);
-    const juros = Number(frm.inPercentualJuros.value);
-    const multa = Number(frm.inPercentualMulta.value);
+    const valor = Number(frm.inValorConta.value);
+    const dataVencimento = frm.inDataVencimento.value;
+    const dataHoje = new Date();
+    const dataVenc = new Date();
+    
+    
+    const partes = dataVencimento.split("-");
+    dataVenc.setDate(Number(partes[2]));
+    dataVenc.setMonth(Number(partes[1]) - 1);
+    dataVenc.setFullYear(Number(partes[0]));
+    
 
-    const valorAtual = 
+
+    const atraso = dataVenc - dataHoje;
+    console.log(atraso);
+    let multa = 0;
+    let juros = 0;
+
+    if(atraso > 0){
+        const dias = atraso/86400000;
+        multa = valor * taxaMulta;
+        juros = valor * taxaJuros * dias;
+    }
+    
+    const total = valor + multa + juros;
+
+    frm.inMulta.value = multa.toFixed(2);
+    frm.inJuros.value = juros.toFixed(2);
+
+    result.textContent = "Valor total: " + total.toFixed(2);
+    
 })
