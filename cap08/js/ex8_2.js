e258cd2a5faa07eef248dffa15569122240c7c38
Fabricio <fabriciozulian@outlook.com>
2025-04-10 12:41:31 -0300

    fix: ajustando calculo do exercicio 8_2

--- cap08/js/ex8_2.js
+++ cap08/js/ex8_2.js
@@ -19,11 +19,11 @@ function calcula(anoFabricacao, preco){
     situacao = anoFabricacao == 2025 ? "Novo" : 2025 - anoFabricacao <= 2 ? "Seminovo" : "Usado";
     if(situacao == "Novo"){
         let entrada = preco/2;
-        let parcela = (preco/2)/12;
+        let parcela = (preco/2)/10;
         return [entrada.toFixed(2), parcela.toFixed(2), situacao];
     } else {
         let entrada = preco*.3;
-        let parcela = (preco -(preco*.3))/12;
+        let parcela = (preco -(preco*.3))/10;
         return [entrada.toFixed(2), parcela.toFixed(2), situacao];
     }
 }
