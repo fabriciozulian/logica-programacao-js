c41f48abe2da97f1180160890078b4f9d6dffbce
Fabricio Zulian <fabriciozulian@outlook.com>
2025-04-09 23:10:07 -0300

    Adicionando exercicio 8_2

--- cap08/js/ex8_2.js
+++ cap08/js/ex8_2.js
@@ -1,4 +1,5 @@
-const frm = document.querySelector("form")
+const frm = document.querySelector("form");
+const result = document.querySelector("#resposta");
 
 
 frm.btCalcula.addEventListener("click", (e) => {
@@ -7,11 +8,22 @@ frm.btCalcula.addEventListener("click", (e) => {
     const modelo = frm.inModelo.value;
     const anoFabricacao = frm.inAnoFabricacao.value;
     const preco = frm.inPreco.value;
-
-
-
+    const [entrada, parcela, situacao] = calcula(anoFabricacao, preco);
+    let calculo = `${modelo} - ${situacao} \n Entrada R$: ${entrada} \n +10x de R$: ${parcela}`;
+    result.innerText = calculo;
+    
 })
 
 
-const situacaoModelo = (anoModelo) = (anoModelo == 2025 ? "Novo" : (2025 - anoModelo) <= 2 ? "Seminovo" : Usado);
-const calcula = (anoModelo, precoModelo) = (situacaoModelo(anoModelo) == "Novo" ? )
+function calcula(anoFabricacao, preco){
+    situacao = anoFabricacao == 2025 ? "Novo" : 2025 - anoFabricacao <= 2 ? "Seminovo" : "Usado";
+    if(situacao == "Novo"){
+        let entrada = preco/2;
+        let parcela = (preco/2)/12;
+        return [entrada.toFixed(2), parcela.toFixed(2), situacao];
+    } else {
+        let entrada = preco*.3;
+        let parcela = (preco -(preco*.3))/12;
+        return [entrada.toFixed(2), parcela.toFixed(2), situacao];
+    }
+}
